d742239a57b757a77d84414a2d67f07677f4ff9a
Jacob <jacobryanstevenson@gmail.com>
2022-06-04 17:15:48 -0500

    fix

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,29 +8,28 @@ import {
 } from '@mui/material';
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { useState, useEffect } from 'react';
 import Layout from '../../components/Layout';
 import classes from '../../utils/classes';
 import client from '../../utils/client';
 import { urlFor } from '../../utils/image';
+import { useState, useEffect } from 'react';
 
 export default function ProductScreen(props) {
   const { slug } = props;
   const [state, setState] = useState({
     product: null,
     loading: true,
-    erorr: '',
+    error: '',
   });
-
   const { product, loading, error } = state;
   useEffect(() => {
     const fetchData = async () => {
       try {
         const product = await client.fetch(
-          `*[_type == "product" && slug.current == "$slug"][0]`,
+          `
+              *[_type == "product" && slug.current == $slug][0]`,
           { slug },
         );
-
         setState({ ...state, product, loading: false });
       } catch (err) {
         setState({ ...state, error: err.message, loading: false });
@@ -73,8 +72,6 @@ export default function ProductScreen(props) {
 
 export function getServerSideProps(context) {
   return {
-    props: {
-      slug: context.params.slug,
-    },
+    props: { slug: context.params.slug },
   };
 }
