982e08a02a0707101fb64dae18b23c2f0c398896
Jacob <jacobryanstevenson@gmail.com>
2022-06-03 19:25:46 -0500

    mighterror

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,19 @@
+import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
 import {
   AppBar,
   CssBaseline,
   Link,
-  Toolbar,
   Typography,
+  Toolbar,
   Box,
   Container,
 } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
 import Head from 'next/head';
 import NextLink from 'next/link';
 import classes from '../utils/classes';
 
-const Layout = ({ title, description, children }) => {
+export default function Layout({ title, description, children }) {
   const theme = createTheme({
     components: {
       MuiLink: {
@@ -35,7 +35,6 @@ const Layout = ({ title, description, children }) => {
       },
     },
     palette: {
-      mode: 'light',
       primary: {
         main: '#f0c000',
       },
@@ -44,37 +43,35 @@ const Layout = ({ title, description, children }) => {
       },
     },
   });
+
   return (
     <>
       <Head>
         <title>
           {title ? `${title} - Handmade By Design` : 'Handmade By Design'}
         </title>
-        {description && <meta name="description" content={description} />}
+        {description && <meta name="description" content={description}></meta>}
       </Head>
-
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar position="static" sx={classes.appbar}>
           <Toolbar sx={classes.toolbar}>
-            <NextLink href="/" passHref>
-              <Link>
-                <Typography sx={classes.brand}>Handmade By Design</Typography>
-              </Link>
-            </NextLink>
+            <Box display="flex" alignItems="center">
+              <NextLink href="/" passHref>
+                <Link>
+                  <Typography sx={classes.brand}>Handmade By Design</Typography>
+                </Link>
+              </NextLink>
+            </Box>
           </Toolbar>
         </AppBar>
         <Container component="main" sx={classes.main}>
           {children}
         </Container>
-        <Box component="footer">
-          <Typography sx={classes.footer}>
-            All rights reserved. Handmade by Design &#169; 2022
-          </Typography>
+        <Box component="footer" sx={classes.footer}>
+          <Typography>All rights reserved. Sanity Amazona.</Typography>
         </Box>
       </ThemeProvider>
     </>
   );
-};
-
-export default Layout;
+}
