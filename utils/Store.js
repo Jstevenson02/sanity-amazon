4c678333780f6be240c85a388b48c3c0e3809295
Jacob <jacobryanstevenson@gmail.com>
2022-06-10 16:25:22 -0500

    commit

--- utils/Store.js
+++ utils/Store.js
@@ -11,7 +11,7 @@ const initialState = {
       : [],
     shippingAddress: Cookies.get('shippingAddress')
       ? JSON.parse(Cookies.get('shippingAddress'))
-      : [],
+      : {},
     paymentMethod: Cookies.get('paymentMethod')
       ? Cookies.get('paymentMethod')
       : '',
@@ -47,23 +47,35 @@ function reducer(state, action) {
       Cookies.set('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'CART_CLEAR':
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
+
     case 'USER_LOGIN':
       return { ...state, userInfo: action.payload };
     case 'USER_LOGOUT':
       return {
         ...state,
         userInfo: null,
-        cart: { cartItems: [], shippingAddress: [] },
+        cart: {
+          cartItems: [],
+          shippingAddress: {},
+        },
       };
     case 'SAVE_SHIPPING_ADDRESS':
       return {
         ...state,
-        cart: { ...state.cart, shippingAddress: action.payload },
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload,
+        },
       };
-    case 'SAVE_PAYMENT_METHOD':
+    case 'SVAE_PAYMENT_METHOD':
       return {
         ...state,
-        cart: { ...state.cart, paymentMethod: action.payload },
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
+        },
       };
     default:
       return state;
